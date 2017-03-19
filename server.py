a8c69a4823f73a5e18787c2631f4af37df9706b7
Robin Hellgren <rhellgren@icloud.com>
2017-03-19 16:53:01 +0100

    POST new messages

--- server.py
+++ server.py
@@ -1,31 +1,52 @@
 from flask import Flask, url_for, request, json, Response
+import datetime, time
 app = Flask(__name__)
 
 @app.route('/')
 def api_root():
     return 'Welcome'
 
+@app.route('/messages/<user>/write', methods = ['POST'])
+def api_write_new_message(user):
+    if request.headers['Content-Type'] == 'application/json':
+        data = json.loads(request.data)
+        new_message = {
+            'id': next_id,
+            'from': user,
+            'to': data['to'],
+            'content': data['content'],
+            'read': False,
+            'timestamp': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        messages.append(new_message)
+        return_message = "The message is on it's way!"
+    else:
+        return_message = "Sorry, format not supported :-("
+
+    return return_message
+
 @app.route('/messages/<user>/new', methods = ['GET'])
 def api_get_new_messages(user):
-    result = ""
-    for message in messages:
-        if message['to'] == user and message['read'] == False:
-            result += message_json_to_string(message)
-
-    resp = Response(result, status=200, mimetype='application/json')
+    result = messages_for_user(user, True)
+    resp = Response(result, status=200, mimetype='text/plain')
     return resp
-
-
 
 @app.route('/messages/<user>/all', methods = ['GET'])
 def api_get_all_messages(user):
+    result = messages_for_user(user, False)
+    resp = Response(result, status=200, mimetype='text/plain')
+    return resp
+
+def messages_for_user(user, only_unread):
     result = ""
     for message in messages:
         if message['to'] == user:
-            result += message_json_to_string(message)
-
-    resp = Response(result, status=200, mimetype='application/json')
-    return resp
+            if only_unread:
+                if not message['read']:
+                    result += message_json_to_string(message)
+            else:
+                result += message_json_to_string(message)
+    return result
 
 def message_json_to_string(message):
     result = ""
@@ -38,7 +59,7 @@ def message_json_to_string(message):
     return result
 
 if __name__ == '__main__':
-    global messages
+    global messages, next_id
     messages = [
         {
             'id': 0,
@@ -46,7 +67,7 @@ if __name__ == '__main__':
             'to': 'Hellgren',
             'content': 'This is what I want to tell you!',
             'read': False,
-            'timestamp': '2017-03-19T13:08:21'
+            'timestamp': '2017-03-19 13:08:21'
         },
         {
             'id': 1,
@@ -54,7 +75,7 @@ if __name__ == '__main__':
             'to': 'Optima',
             'content': 'This is the second message',
             'read': False,
-            'timestamp': '2017-03-18T11:18:00'
+            'timestamp': '2017-03-18 11:18:00'
         },
         {
             'id': 2,
@@ -62,7 +83,8 @@ if __name__ == '__main__':
             'to': 'Optima',
             'content': 'Third message coming up!',
             'read': False,
-            'timestamp': '2017-02-11T03:59:59'
+            'timestamp': '2017-02-11 03:59:59'
         }
     ]
+    next_id = 3
     app.run()
