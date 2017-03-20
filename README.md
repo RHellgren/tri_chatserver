791dec3d9238c3787556f177d5ff445c257cb447
Robin Hellgren <rhellgren@icloud.com>
2017-03-20 10:44:32 +0100

    Updated README, minor code fixes

--- README.md
+++ README.md
@@ -1,40 +1,43 @@
-# tri_chatserver - Simple chat server in Python, handles GET and POST requests (see commands below).
+# tri_chatserver
+*Simple chat server in Python, handles GET and POST requests.*
 Built to work in Python 2.7.10 with Flask 0.12 (see requirements.txt).
 
 Simply build server with: python server.py
 
-The following endpoints is implemented, with a short explanation and a curl example command :
-  [GET] /messages Returns all messages currently in the system
+The following endpoints is implemented, with a short explanation and a curl example command:<br />
+
+  [GET] '/messages'<br />
+  Returns all messages currently in the system<br />
   > curl "http://127.0.0.1:5000/messages"
   
-  [GET] /messages/<id>
-  Returns the message specified by id, or 'No message with that ID' if non existent.
+  [GET] '/messages/<id>'<br />
+  Returns the message specified by id, or 'No message with that ID' if non existent.<br />
   > curl "http://127.0.0.1:5000/messages/0"
   
-  [GET] /messages/<id>/remove
-  Removes the specified message, returns 'Message deleted' if successful
-  or 'Message could not be deleted, wrong id?' othewise.
+  [GET] '/messages/<id>/remove'<br />
+  Removes the specified message, returns 'Message deleted' if successful<br />
+  or 'Message could not be deleted, wrong id?' othewise.<br />
   > curl "http://127.0.0.1:5000/messages/0/remove"
   
-  [GET] /messages/<user>/all
-  Returns all messages sent to the user specified, empty string if no messages are present in the system.
-  Can also filter out messages by id-range or timestamp-range (inclusive), examples shown below.
-  > curl "http://127.0.0.1:5000/messages/Hellgren/all"
-  > curl "http://127.0.0.1:5000/messages/Hellgren/all?start_index=0&end_index=3"
-  > curl "http://127.0.0.1:5000/messages/Hellgren/all?before=2017-03-20T10:31:45&after=2017-03-20T10:31:30"
+  [GET] '/messages/<user>/all'<br />
+  Returns all messages sent to the user specified, empty string if no messages are present in the system.<br />
+  Can also filter out messages by id-range or timestamp-range (inclusive), examples shown below.<br />
+  > curl "http://127.0.0.1:5000/messages/Hellgren/all"<br />
+  > curl "http://127.0.0.1:5000/messages/Hellgren/all?start_index=0&end_index=3"<br />
+  > curl "http://127.0.0.1:5000/messages/Hellgren/all?before=2017-03-20T10:31:45&after=2017-03-20T10:31:30"<br />
   
-  [GET] '/messages/<user>/new'
+  [GET] '/messages/<user>/new'<br />
   Returns new messages sent to the user specified since the last update (i.e. unread messages),
   empty string if no messages are present in the system.
   > curl "http://127.0.0.1:5000/messages/Hellgren/new"
   
-  [POST] /messages/<user>/write
-  Post a new message to the system from the user specified, either takes plain-text input
-  (specifying the reciever with '#' sign) or JSON input, examples shown below.
-  > curl -H "Content-type: text/plain; charset=UTF-8" -X POST "http://127.0.0.1:5000/messages/Hellgren/write" -d "#Robin Hej Världen"
-  > curl -H "Content-type: application/json" -X POST "http://127.0.0.1:5000/messages/Hellgren/write" -d '{"to":"Robin", "content": "Hej världen"}'
+  [POST] '/messages/<user>/write'
+  Post a new message to the system from the user specified, either takes plain-text input<br />
+  (specifying the reciever with '#' sign) or JSON input, examples shown below.<br />
+  > curl -H "Content-type: text/plain; charset=UTF-8" -X POST "http://127.0.0.1:5000/messages/Hellgren/write" -d "#Robin Hej Världen"<br />
+  > curl -H "Content-type: application/json" -X POST "http://127.0.0.1:5000/messages/Hellgren/write" -d '{"to":"Robin", "content": "Hej världen"}'<br />
   
-  [GET] /messages/remove_multiple
-  Removes messages within a range, specified by the arguments 'start_index' and 'end_index' (inclusive),
-  returns 'Messages removed' if successful and 'You need to specify which messages to remove' otherwise.
-  > curl "http://127.0.0.1:5000/messages/remove_multiple?start_index=0&end_index=3"
+  [GET] '/messages/remove_multiple'<br />
+  Removes messages within a range, specified by the arguments 'start_index' and 'end_index' (inclusive),<br />
+  returns 'Messages removed' if successful and 'You need to specify which messages to remove' otherwise.<br />
+  > curl "http://127.0.0.1:5000/messages/remove_multiple?start_index=0&end_index=3"<br />
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, json, Response
+from flask import Flask, request, json, Response
 import datetime, time
 app = Flask(__name__)
 
@@ -8,43 +8,12 @@ def api_root():
     return 'Welcome'
 
 
-@app.route('/messages/<user>/write', methods=['POST'])
-def api_write_new_message(user):
-    if request.headers['Content-Type'] == 'application/json':
-        data = json.loads(request.data)
-        new_message = {
-            'id': NEXT_ID,
-            'from': user,
-            'to': data['to'],
-            'content': data['content'],
-            'read': False,
-            'timestamp': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        }
-        MESSAGES.append(new_message)
-        return_message = "The message is on it's way!"
-        increment_id()
-    else:
-        return_message = "Sorry, format not supported :-("
-
-    return return_message
-
-
-@app.route('/messages/<user>/new', methods=['GET'])
-def api_get_new_messages(user):
-    result = get_all_messages_for_user(user, True, None, None, None, None)
-    resp = Response(result, status=200, mimetype='text/plain')
-    return resp
-
-
-@app.route('/messages/<user>/all', methods=['GET'])
-def api_get_all_messages(user):
-    if 'before' and 'after' in request.args:
-        result = get_all_messages_for_user(user, False, request.args['after'], request.args['before'], None, None)
-    elif 'start_index' and 'end_index' in request.args:
-        result = get_all_messages_for_user(user, False, None, None, request.args['start_index'], request.args['end_index'])
-    else:
-        result = get_all_messages_for_user(user, False, None, None, None, None)
-    resp = Response(result, status=200, mimetype='text/plain')
+@app.route('/messages', methods=['GET'])
+def api_get_all_messages():
+    return_string = ""
+    for message in MESSAGES:
+        return_string += message_json_to_string(message) + "\n"
+    resp = Response(return_string, status=200, mimetype='text/plain')
     return resp
 
 
@@ -65,8 +34,65 @@ def api_remove_message(id):
         MESSAGES.remove(message)
         resp = Response("Message deleted\n", status=200, mimetype='text/plain')
     else:
-        resp = Response("No message with that ID\n", status=200, mimetype='text/plain')
+        resp = Response("Message could not be deleted, wrong id?\n", status=200, mimetype='text/plain')
     return resp
+
+
+@app.route('/messages/<user>/all', methods=['GET'])
+def api_get_all_user_messages(user):
+    if 'before' and 'after' in request.args:
+        result = get_all_messages_for_user(user, False, request.args['after'], request.args['before'], None, None)
+    elif 'start_index' and 'end_index' in request.args:
+        result = get_all_messages_for_user(user, False, None, None, request.args['start_index'], request.args['end_index'])
+    else:
+        result = get_all_messages_for_user(user, False, None, None, None, None)
+    resp = Response(result, status=200, mimetype='text/plain')
+    return resp
+
+
+@app.route('/messages/<user>/new', methods=['GET'])
+def api_get_new_messages(user):
+    result = get_all_messages_for_user(user, True, None, None, None, None)
+    resp = Response(result, status=200, mimetype='text/plain')
+    return resp
+
+
+@app.route('/messages/<user>/write', methods=['POST'])
+def api_write_new_message(user):
+    if request.headers['Content-Type'] == 'application/json':
+        data = json.loads(request.data)
+        new_message = {
+            'id': NEXT_ID,
+            'from': user,
+            'to': data['to'],
+            'content': data['content'],
+            'read': False,
+            'timestamp': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        MESSAGES.append(new_message)
+        return_message = "The message is on it's way!"
+        increment_id()
+    elif request.headers['Content-Type'] == 'text/plain; charset=UTF-8':
+        data = request.get_data().decode('utf-8')
+        start = data.index('#') + len('#')
+        end = data.index(' ', start)
+        to_user = data[start:end]
+        message = data[end+1:]
+        new_message = {
+            'id': NEXT_ID,
+            'from': user,
+            'to': to_user,
+            'content': message,
+            'read': False,
+            'timestamp': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+        }
+        MESSAGES.append(new_message)
+        return_message = "The message is on it's way!"
+        increment_id()
+    else:
+        return_message = "Sorry, format not supported :-("
+
+    return return_message
 
 
 @app.route('/messages/remove_multiple', methods=['GET'])
@@ -80,9 +106,9 @@ def api_remove_multiple_messages():
             if int(message['id']) > end_index or int(message['id']) < start_index:
                 messages_to_keep.append(message)
         MESSAGES = messages_to_keep
-        resp = Response("Messages removed", status=200, mimetype='text/plain')
+        resp = Response("Messages removed\n", status=200, mimetype='text/plain')
     else:
-        resp = Response("You need to specify which messages to remove", status=200, mimetype='text/plain')
+        resp = Response("You need to specify which messages to remove\n", status=200, mimetype='text/plain')
     return resp
 
 
